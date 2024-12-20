d9f4270888af21edc709f48aa51a15968dc0d309
엄채연 EOM CHAE YEON <43158518+sorimchuku@users.noreply.github.com>
2024-12-20 17:25:36 +0900

    프로젝트 폴더 생성

---
+++ backend/src/main/java/com/busanit/myplannerbackend/MyPlannerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.busanit.myplannerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPlannerBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MyPlannerBackendApplication.class, args);
+  }
+
+}
