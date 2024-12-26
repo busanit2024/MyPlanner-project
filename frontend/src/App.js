9e499374137a5a97ea22097e402a747456e88753
엄채연 EOM CHAE YEON <43158518+sorimchuku@users.noreply.github.com>
2024-12-26 12:25:25 +0900

    메인 레이아웃 및 전역 변수 설정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Layout from './ui/Layout';
+import MainPage from './components/main/MainPage';
+import GlobalStyles from './styles/GlobalStyles.styles';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+return (
+    <Router>
+      <GlobalStyles />
+      <Routes>
+        <Route element={<Layout />} >
+          <Route index element={<MainPage />} />
+          <Route path="calendar" element={<MainPage />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
