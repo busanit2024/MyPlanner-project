827c3d3a997f6df44138837a9d02336b4ab3a5b3
MJ-davie <MJnotZ0@gmail.com>
2024-12-27 11:06:53 +0900

    ChatTitle Component

--- frontend/src/components/chat/ChatPage.jsx
+++ frontend/src/components/chat/ChatPage.jsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import InputChat from "./chatComponent/InputChat";
+import ChatTitle from "./chatComponent/ChatTitle";
+import defaultProfile from "../../images/defaultProfile.png";
 
 const ChatContainer = styled.div`
   display: flex;
@@ -10,7 +12,7 @@ const ChatContainer = styled.div`
 `;
 
 const ChatList = styled.div`
-width: 30%;  
+width: 40%;  
 border-right: 1px solid #d9d9d9;
 padding: 24px;
 padding-right: 23px;
@@ -27,11 +29,6 @@ padding: 24px;
 height : 100%;
 `;
 
-const ChatTitle = styled.div`
-padding-bottom: 20px;
-border-bottom: 1px solid #d9d9d9;
-`;
-
 const ChatMessages = styled.div`
 flex: 1;
 overflow-y: auto;
@@ -45,16 +42,29 @@ const ChatInput = styled.div`
 
 
 export default function ChatPage() {
+    const profileImage = defaultProfile;
+    const userName = "닉네임";
+    const userEmail = "test@test.com";
+
     return (
         <ChatContainer>
             <ChatList>
                 <h2>채팅목록</h2>
             </ChatList>
             <ChatRoom>
-                <ChatTitle>
-                    <h3>채팅상대</h3>
-                </ChatTitle>
-
+                <ChatTitle 
+                    profileImage={profileImage} 
+                    userName={userName} 
+                    userEmail={userEmail}
+                    style={{
+                        paddingBottom: '20px',
+                        borderBottom: '1px solid #d9d9d9',
+                        width: 'calc(100% + 48px)',
+                        margin: '0 -24px',
+                        paddingLeft: '24px',
+                        paddingRight: '24px'
+                    }}
+                />
                 <ChatMessages>
                     <h4>채팅 내용</h4>
                 </ChatMessages>
---
+++ frontend/src/components/chat/chatComponent/ChatTitle.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const TitleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  width:100%;
+`;
+
+const ProfileImage = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+`;
+
+const UserInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const UserName = styled.span`
+  font-weight: bold;
+`;
+
+const UserEmail = styled.span`
+  color: #666;
+  font-size: 0.9em;
+`;
+
+const ChatTitle = ({ profileImage, userName, userEmail, ...props }) => {
+  return (
+    <TitleContainer {...props}>
+      <ProfileImage src={profileImage} alt="프로필 이미지" />
+      <UserInfo>
+        <UserName>{userName}</UserName>
+        <UserEmail>{userEmail}</UserEmail>
+      </UserInfo>
+    </TitleContainer>
+  );
+};
+
+export default ChatTitle;
