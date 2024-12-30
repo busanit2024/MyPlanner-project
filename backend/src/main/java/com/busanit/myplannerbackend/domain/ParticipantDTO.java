982a5ac2b0c96e1a8ad408cb0f68e78af5587992
dduhell <suhyun97a@gmail.com>
2024-12-30 15:13:00 +0900

    채팅방 backend 생성

---
+++ backend/src/main/java/com/busanit/myplannerbackend/domain/ParticipantDTO.java
@@ -0,0 +1,11 @@
+package com.busanit.myplannerbackend.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ParticipantDTO {
+    private String id;
+    private String status;
+}
