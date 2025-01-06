1a0332253133e950672871ec27179dc85e1d0b91
엄채연 EOM CHAE YEON <43158518+sorimchuku@users.noreply.github.com>
2025-01-06 17:39:12 +0900

    그룹 페이지 레이아웃 뼈대

---
+++ frontend/src/components/group/GroupListItem.jsx
@@ -0,0 +1,102 @@
+import styled from "styled-components";
+
+export default function GroupListItem({ group }) {
+  return(
+    <Container src={group?.profileImageUrl}>
+      <GroupHeader>
+        <div className="group-image">
+          <img src={group?.profileImageUrl} alt="그룹 이미지" />
+        </div>
+        <div className="group-name">{group?.name ?? "그룹명"}</div>
+      </GroupHeader>
+      <GroupInfo>
+        <span className="next-schedule">다음 일정</span>
+        <div className="schedule-info">
+          <span className="date">{"2015-00-00"}</span>
+          <span className="schedule-title">{"일정 이름"}</span>
+        </div>
+      </GroupInfo>
+
+    </Container>
+
+  );
+}
+
+const Container = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  width: 100%;
+  height: 240px;
+  padding: 36px;
+  box-sizing: border-box;
+  border-radius: 8px;
+  cursor: pointer;
+  background-image: url(${(props) => props.src});
+  background-size: cover;
+  background-position: center;
+  background-color: #fff;
+  overflow: hidden;
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.1);
+    z-index: 1;
+  }
+
+  & > * {
+    position: relative;
+    z-index: 2;
+  }
+`;
+
+const GroupHeader = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+
+  & .group-image {
+    width: 64px;
+    height: 64px;
+    border-radius: 50%;
+    background-color: var(--light-gray);
+
+    & img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      object-fit: cover;
+    }
+  }
+
+  & .group-name {
+    font-size: 18px;
+    font-weight: bold;
+  }
+`;
+
+const GroupInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+
+  & .schedule-info {
+    display: flex;
+    gap: 4px;
+  }
+
+  & .next-schedule {
+    font-size: 16px;
+    font-weight: bold;
+  } 
+
+  & .date {
+    color: var(--mid-gray);
+  }
+`;
--- frontend/src/components/group/GroupPage.jsx
+++ frontend/src/components/group/GroupPage.jsx
@@ -1,9 +1,33 @@
+import { useState } from "react";
 import styled from "styled-components";
+import { useAuth } from "../../context/AuthContext";
+import GroupListItem from "./GroupListItem";
 
 export default function GroupPage() {
+  const { user, loading } = useAuth();
+  const [searchType, setSearchType] = useState('my');
+  const [myGroups, setMyGroups] = useState([]);
+  const [exploreGroups, setExploreGroups] = useState([]);
+  const [listLoading, setListLoading] = useState(true);
+  const [page, setPage] = useState(0);
+  const [hasNext, setHasNext] = useState(false);
+
   return (
     <Container>
-      그룹
+      <SearchTypeWrap>
+        <div className={`search-type ${searchType === 'my' ? 'active' : ''}`} onClick={() => setSearchType("my")}>
+          <span >내 그룹</span>
+          <div></div>
+        </div>
+        <div className={`search-type ${searchType === 'explore' ? 'active' : ''}`} onClick={() => setSearchType("explore")}>
+          <span>탐색</span>
+          <div></div>
+        </div>
+      </SearchTypeWrap>
+      <SearchResultList>
+        <GroupListItem />
+
+      </SearchResultList>
     </Container>
   );
 }
@@ -12,4 +36,63 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
+  height: 100%;
+`;
+
+const SearchTypeWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 4px;
+  height: 64px;
+  border-bottom: 1px solid var(--light-gray);
+  flex-shrink: 0;
+  width: 100%;
+
+  & .search-type {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    flex-grow: 1;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+
+    & span {
+      font-size: 18px;
+      justify-self: center;
+      margin: auto;
+    }
+
+    & div {
+      width: 120px;
+      height: 2px;
+      justify-self: flex-end;
+      background-color: var(--light-gray);
+    }
+
+    &.active div {
+      background-color: var(--primary-color);
+    }
+
+    &.active span {
+      font-weight: bold;
+    }
+  }
+`;
+
+const SearchResultList = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  padding: 24px 128px;
+  box-sizing: border-box;
+  width: 100%;
+
+  & .no-result {
+    margin-top: 64px;
+    font-size: 18px;
+    text-align: center;
+    color: var(--mid-gray);
+  }
 `;
