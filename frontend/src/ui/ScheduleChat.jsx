6a52bcd5bd816ded0db29d6b00a403118716bfc4
dduhell <suhyun97a@gmail.com>
2025-01-15 14:41:10 +0900

    컴포넌트 생성 중

---
+++ frontend/src/ui/ScheduleChat.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+
+const ScheduleWrapper = styled.div`
+  background-color: #ffffff;
+  border-radius: 15px;
+  border: 1px solid #B6B6B6;
+  padding: 10px;
+`
+
+const Title = styled.div`
+  font-size: 14px;
+  font-style: bold;
+  color: #000000;
+  justify-content: left;
+  border-bottom: 1px solid #B6B6B6;
+`
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: left;
+  padding: 10px;
+  font-size: 14px;
+  color: #000000;
+  border-bottom: 1px solid #B6B6B6;
+`
+
+export default SchduleChat;
